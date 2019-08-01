c0ea89a8429a9b94c1a5f3cc21bb6417eab2b066
Damián Pumar <damianpumar@gmail.com>
2019-08-01 15:15:52 +0200

    All images loading async

--- js/main.js
+++ js/main.js
@@ -24,14 +24,15 @@ var maginNumber = 31557600000;
 var utilities = {
 
     loadImagesAsync: function loadImagesAsync() {
-        // Page is loaded
 
         var objects = document.getElementsByClassName('asyncImage');
+
         Array.from(objects).map(function (item) {
-            // Start loading image
+
             var img = new Image();
+
             img.src = item.dataset.src;
-            // Once image is loaded replace the src of the HTML element
+
             img.onload = function () {
                 item.classList.remove('asyncImage');
                 return item.nodeName === 'IMG' ? item.src = item.dataset.src : item.style.backgroundImage = 'url(' + item.dataset.src + ')';
